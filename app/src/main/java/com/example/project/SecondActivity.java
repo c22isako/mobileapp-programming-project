5921fa7d66835ca368cacfccf40d5fdf97660e7c
c22isako <c22isako@student.his.se>
2024-05-08 16:01:57 +0200

    Created an activity, SecondActivity, and modified build.gradle

---
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.example.project;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_second);
+
+            Bundle extras = getIntent().getExtras();
+            if (extras != null) {
+                String type = extras.getString("potatoType");
+                int number = extras.getInt("anyNumber");
+
+                TextView ExtrasTextView = findViewById(R.id.secondActivityTextView);
+                ExtrasTextView.setText("Potatos are great. Especially the type " + type + ". And Atleast " + number + " Each meal.");
+
+            }
+        }
+}
